773ac4e0b261f18adc711e3529c1dce013a879fb
hari1vkp <rkrish2k9@gmail.com>
2024-11-24 23:29:42 +0530

    fin no

--- Fib.java
+++ Fib.java
@@ -4,18 +4,24 @@ public class Fib {
     public static void main(String[] args) {
 
 
-        int[] fib = new int[10];
-        fib[0] = 0;
-        fib[1] = 1;
+        int[] fib = new int[100];
         Fib h=new Fib();
-        System.out.println(fib[0]);
-        System.out.println(fib[1]);
         h.fib(fib);
     }
     void fib(int []arr){
-        for(int i=2;i<100;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-            System.out.println(arr[i]);
+        for(int i=0;i<10;i++){
+            if(i==0 || i==1){
+                arr[i]=i;
+                System.out.println(arr[i]);
+
+            }
+            else {
+                arr[i] = arr[i - 1] + arr[i - 2];
+                System.out.println(arr[i]);
+            }
         }
+
+
     }
+
 }
